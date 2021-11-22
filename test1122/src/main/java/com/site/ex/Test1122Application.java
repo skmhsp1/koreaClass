59daa88ff0ae68c3c4c32c8cc5b4922237c54b85
koreavc <koreavc@DESKTOP-3FNCEU3>
2021-11-22 09:33:44 +0900

    20211122 sample1.

---
+++ test1122/src/main/java/com/site/ex/Test1122Application.java
@@ -0,0 +1,13 @@
+package com.site.ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test1122Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test1122Application.class, args);
+	}
+
+}
